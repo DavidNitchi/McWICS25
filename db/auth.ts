d2ae6ec65fbc1b6dfd8e525faa6c75261eb9ec75
Denis Tsariov <tsariov.den@gmail.com>
2025-01-18 18:34:41 -0500

    basic signup

--- db/auth.ts
+++ db/auth.ts
@@ -1,48 +1,44 @@
+"use server";
+
 import bcrypt from "bcrypt";
 import { z } from "zod";
 import { addUser } from "./query";
 
 export async function signup(state: FormState, formData: FormData) {
-  console.log("hello1");
-
   // Validate form fields
   const validatedFields = SignupFormSchema.safeParse({
     name: formData.get("name"),
     email: formData.get("email"),
     password: formData.get("password"),
   });
-  console.log("2");
   // If any form fields are invalid, return early
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
     };
   }
-  console.log("3");
   const { name, email, password } = validatedFields.data;
   // e.g. Hash the user's password before storing it
   //const hashedPassword = await bcrypt.hash(password, 10);
   const hashedPassword = "hello";
 
-  console.log("hello");
   // 3. Insert the user into the database or call an Auth Library's API
   const data = await addUser({
     name: name,
     email: email,
     password: hashedPassword,
   });
+  
+  const user = data;
 
-  console.log(data);
-  // const user = data;
-
-  // if (!user) {
-  //   return {
-  //     message: "An error occurred while creating your account.",
-  //   };
-  // }
+  if (!user) {
+    return {
+      message: "An error occurred while creating your account.",
+    };
+  }
 }
 
-export const SignupFormSchema = z.object({
+const SignupFormSchema = z.object({
   name: z
     .string()
     .min(2, { message: "Name must be at least 2 characters long." })
@@ -59,7 +55,7 @@ export const SignupFormSchema = z.object({
     .trim(),
 });
 
-export type FormState =
+type FormState =
   | {
       errors?: {
         name?: string[];
