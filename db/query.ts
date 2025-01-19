3666b3cf980b0a7bc51ce61d1e8c8bf37730a20f
DavidNitchi <davidnitchi@icloud.com>
2025-01-18 19:27:24 -0500

    Adding returns to queries

--- db/query.ts
+++ db/query.ts
@@ -6,55 +6,64 @@ import { eq } from "drizzle-orm";
 const db = drizzle({ schema });
 
 export async function getUser(userEmail: string) {
-  await db.query.usersTable.findFirst({
+  const ret = await db.query.usersTable.findFirst({
     where: eq(schema.usersTable.email, userEmail),
   });
+  return ret
 }
 
 export async function getEducation(userEmail: string) {
-  await db.query.education.findMany({
+  const ret = await db.query.education.findMany({
     where: eq(schema.education.userId, userEmail),
   });
+  return ret
 }
 
 export async function getworkExperience(userEmail: string) {
-  await db.query.workExperience.findMany({
+  const ret = await db.query.workExperience.findMany({
     where: eq(schema.workExperience.userId, userEmail),
   });
+  return ret
 }
 
 export async function getProject(userEmail: string) {
-  await db.query.project.findMany({
+  const ret = await db.query.project.findMany({
     where: eq(schema.project.userId, userEmail),
   });
+  return ret
 }
 
 export async function getExtracurricular(userEmail: string) {
-  await db.query.extraCurricular.findMany({
+  const ret = await db.query.extraCurricular.findMany({
     where: eq(schema.extraCurricular.userId, userEmail),
   });
+  return ret
 }
 
 type NewUser = typeof schema.usersTable.$inferInsert;
 export async function addUser(user: NewUser) {
-  const data = await db.insert(schema.usersTable).values(user);
-  return data;
+  const ret = await db.insert(schema.usersTable).values(user);
+  return ret;
 }
 
 type NewEducation = typeof schema.education.$inferInsert;
 export async function addEducation(education: NewEducation) {
-  return db.insert(schema.education).values(education);
+  const ret = await db.insert(schema.education).values(education);
+  return ret
 }
 
 type NewProject = typeof schema.project.$inferInsert;
 export async function addProject(project: NewProject) {
-  return db.insert(schema.project).values(project);
+  const ret  = await db.insert(schema.project).values(project);
+  return ret
 }
 type NewWork = typeof schema.workExperience.$inferInsert;
 export async function addWorkExperience(work: NewWork) {
-  return db.insert(schema.workExperience).values(work);
+  const ret = await db.insert(schema.workExperience).values(work);
+  return ret
 }
 type NewExtraCurricular = typeof schema.extraCurricular.$inferInsert;
 export async function addExtraCurricular(activity: NewExtraCurricular) {
-  return db.insert(schema.extraCurricular).values(activity);
+  const ret = await db.insert(schema.extraCurricular).values(activity);
+  return ret
 }
